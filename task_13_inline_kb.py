fbed8eb2010ab1cfed7bd643f05dc552d2320f20
gennadysgn <entheogen.latina@gmail.com>
2025-01-18 15:28:06 +0300

    Add files via upload
    Домашнее задание по теме "Инлайн клавиатуры".

---
+++ task_13_inline_kb.py
@@ -0,0 +1,115 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+api = "..."
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = InlineKeyboardMarkup()
+button1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+button2 = InlineKeyboardButton(text='Формулы расчета', callback_data='formulas')
+kb.add(button1)
+kb.add(button2)
+
+start_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Рассчитать'),
+            KeyboardButton(text='Информация')
+        ]
+    ], resize_keyboard=True
+)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer("Рады Вас видеть!", reply_markup=start_menu)
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer("Выберите опцию:", reply_markup=kb)
+
+
+@dp.message_handler(text='Информация')
+async def info(message):
+    await message.answer("Домашнее задание по теме 'Инлайн клавиатуры.'")
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('"Мужчины: (10 x вес (кг) + 6.25 x рост (см) – 5 x возраст (г) + 5) x A" \n'
+                              '"Женщины: (10 x вес (кг) + 6.25 x рост (см) – 5 x возраст (г) – 161) x A"')
+    await call.answer()
+
+
+class UserState(StatesGroup):
+    gender = State()
+    weight = State()
+    height = State()
+    age = State()
+    active = State()
+
+
+@dp.callback_query_handler(text='calories')
+async def gender(call):
+    await call.message.answer("Укажите Ваш пол: мужской - 'м', женский - 'ж'")
+    await call.answer()
+    await UserState.gender.set()
+
+
+@dp.message_handler(state=UserState.gender)
+async def fsm_gender(message, state):
+    await state.update_data(gender=message.text)
+    await message.answer('Укажите Ваш вес')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def fsm_weight(message, state):
+    await state.update_data(weight=message.text)
+    await message.answer('Укажите Ваш рост')
+    await UserState.height.set()
+
+
+@dp.message_handler(state=UserState.height)
+async def fsm_height(message, state):
+    await state.update_data(height=message.text)
+    await message.answer('Укажите Ваш возраст')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def fsm_age(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите коэффициент Вашей активности: \n'
+                         'Минимальная активность: A = 1.2. \n'
+                         'Слабая активность: A = 1.375. \n'
+                         'Средняя активность: A = 1.55. \n'
+                         'Высокая активность: A = 1.725. \n'
+                         'Экстра-активность: A = 1.9')
+    await UserState.active.set()
+
+
+@dp.message_handler(state=UserState.active)
+async def fsm_result(message, state):
+    await state.update_data(active=message.text)
+    data = await state.get_data()
+    w = int(data['weight'])
+    h = int(data['height'])
+    a = int(data['age'])
+    act = float(data['active'])
+    if data['gender'] == 'м':
+        await message.answer(f"Ваша суточная норма {((10 * w) + (6.25 * h) - (5 * a) + 5) * act} калорий.")
+    else:
+        await message.answer(f"Ваша суточная норма {((10 * w) + (6.25 * h) - (5 * a) - 161) * act} калорий.")
+    await state.finish()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
